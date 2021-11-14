52e61c82b2c5260d686abcaba773ccd9066575fc
NOOR <noormuhammed14@gmail.com>
2021-11-14 18:18:13 +0530

    job data designed navbar designed

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Componet/Navbar';
+import ShowJobs from './Componet/ShowJobs';
 
 function App() {
   return (
     <div className="App">
        <Navbar/>
+       <ShowJobs/>
     </div>
   );
 }

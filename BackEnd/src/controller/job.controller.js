0a1a4c7a1711d5efb74f9db92507bb81e80b329c
NOOR <noormuhammed14@gmail.com>
2021-11-14 16:19:07 +0530

    backend post and get operation complete backend complete

---
+++ BackEnd/src/controller/job.controller.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const Job= require("../modals/job.modal");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const job = await Job.create(req.body);
+  return res.status(201).send({ student });
+});
+
+router.get("/", async (req, res) => {
+  const job = await Job.find().lean().exec();
+  return res.status(200).send({ job });
+});
+
+router.get("/:id", async (req, res) => {
+  const job = await Job.findById(req.params.id).lean().exec();
+  return res.status(200).send({ student });
+});
+
+router.patch("/:id", async (req, res) => {
+  const job = await Job.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  })
+    .lean()
+    .exec();
+  return res.status(200).send({job});
+});
+
+router.delete("/:id", async (req, res) => {
+  const job = await Job.findByIdAndRemove(req.params.id);
+  return res.status(200).send({job});
+});
+
+module.exports = router;
--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connect = require("./config/db");
+const jobController=require("./controller/job.controller")
 const app = express();
 const cors = require("cors");
 
@@ -7,6 +8,7 @@ app.use(express.json()); //middleware
 
 app.use(cors());
 
+app.use("/job", jobController);
 app.listen(8000, async () => {
   await connect();
   console.log("Listening on port 8000");
